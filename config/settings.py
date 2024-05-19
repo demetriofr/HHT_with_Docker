7e93decb1fed2f044292ffac03f364d4168242c1
demetriofr <dmitriy.a.frolov@gmail.com>
2024-05-19 14:50:58 +0300

    Add Docker in project.

--- config/settings.py
+++ config/settings.py
@@ -93,9 +93,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('NAME'),
-        'USER': os.getenv('USERNAME'),
-
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
 
